51a1dc556681aa876c3ccc7fec5849621d273562
Alucard17th <eddallal.noureddine@gmail.com>
2024-01-30 17:45:17 +0100

    def

--- resources/views/recruiter/vitrine/vitrine.blade.php
+++ resources/views/recruiter/vitrine/vitrine.blade.php
@@ -328,10 +328,10 @@ nav > ul.pagination > li > a{
                                             <!-- <input type="text" class="form-control" name="fondateurs" id="fondateurs"
                                                 value="{{ isset($entreprise) ? $entreprise->fondateurs : ''}}"> -->
                                             <select id='diacritics' name='fondateurs[]' class='' multiple>
-                                                @if($entreprise->fondateurs != null && isset($entreprise->fondateurs))
-                                                @foreach (json_decode($entreprise->fondateurs, true) as $fondateur)
-                                                    <option value="{{ $fondateur }}" selected>{{ $fondateur }}</option>
-                                                @endforeach
+                                                @if ($entreprise->fondateurs)
+                                                    @foreach (json_decode($entreprise->fondateurs, true) as $fondateur)
+                                                        <option value="{{ $fondateur }}" selected>{{ $fondateur }}</option>
+                                                    @endforeach
                                                 @endif
                                             </select>
                                         </div>
