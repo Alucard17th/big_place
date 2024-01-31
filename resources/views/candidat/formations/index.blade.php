6892f91d6a07b7b9275af12100e70066b25ab90b
Alucard17th <eddallal.noureddine@gmail.com>
2024-01-31 06:52:58 +0100

    dsqd

--- resources/views/candidat/formations/index.blade.php
+++ resources/views/candidat/formations/index.blade.php
@@ -133,9 +133,8 @@ input, select{
                                                 @if($formation->subscribers >= $formation->max_participants)
                                                     <span class="text-danger">Inscription fermée.</span>
                                                 @endif
-                                                @if (!$formation->participants->pluck('id')->contains(Auth::id()) &&
-                                                 !$formation->subscribers >= $formation->max_participants &&
-                                                 !$formation->max_participants > 0
+                                                @if (!$formation->participants->pluck('id')->contains(Auth::id()) 
+                                                 
                                                 )
                                                 <a href="{{ route('candidat.formation.subscribe', $formation->id) }}" type="button" class="bg-btn-seven mb-2 px-2">Je participe</a>
                                                 @endif
