442ca0fe4a11da98c909b2140bbf5e85fa141271
Alucard17th <eddallal.noureddine@gmail.com>
2024-02-08 13:14:31 +0100

    qsdsd

--- app/Http/Controllers/Candidat/OfferController.php
+++ app/Http/Controllers/Candidat/OfferController.php
@@ -134,13 +134,12 @@ class OfferController extends Controller
             }
     
             if (!empty($searchTerm['brut_salary'])) {
-                if(strpos($searchTerm['brut_salary'], '-') !== false){
-                    [$minOfferSalary, $maxOfferSalary] = explode(' - ', $offer->brut_salary);
-                    if(!empty($minOfferSalary) && !empty($maxOfferSalary)){
-                        $score += ($searchTerm['brut_salary'] >= $minOfferSalary && $searchTerm['brut_salary'] <= $maxOfferSalary) ? 10 : 0;
-                    }else{
-                        $score += ($searchTerm['brut_salary'] == $offer->brut_salary) ? 10 : 0;
-                    }
+              
+                [$minOfferSalary, $maxOfferSalary] = explode(' - ', $offer->brut_salary);
+                if(!empty($minOfferSalary) && !empty($maxOfferSalary)){
+                    $score += ($searchTerm['brut_salary'] >= $minOfferSalary && $searchTerm['brut_salary'] <= $maxOfferSalary) ? 10 : 0;
+                }else{
+                    $score += ($searchTerm['brut_salary'] == $offer->brut_salary) ? 10 : 0;
                 }
             }
     
