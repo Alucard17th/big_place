02a704e8dcbea08c7d30d8c3484c440b8bc9fe09
Alucard17th <eddallal.noureddine@gmail.com>
2024-02-15 16:06:55 +0100

    dsfsdf

---
+++ database/migrations/2024_02_15_160612_add_custom_job_to_curriculum_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCustomJobToCurriculumTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('curriculum', function (Blueprint $table) {
+            //
+            $table->string('custom_job')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('curriculum', function (Blueprint $table) {
+            //
+        });
+    }
+}
