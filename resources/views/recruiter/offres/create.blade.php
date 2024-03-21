8a3b9b73bdc66d76de7d3cc1f081b4b5dd25936a
Alucard17th <eddallal.noureddine@gmail.com>
2024-03-21 16:59:59 +0000

    ddqdqsd

--- resources/views/recruiter/offres/create.blade.php
+++ resources/views/recruiter/offres/create.blade.php
@@ -129,7 +129,7 @@
                                     <input type="date" class="form-control" id="start_date" name="start_date"
                                         data-parsley-min-message="La date doit être supérieure ou égale à la date du jour."
                                         data-parsley-errors-container="#custom-error-message"
-                                        data-parsley-error-message="La date doit être supérieure ou égale à la date du jour."
+                                        
                                         required>
                                     <small class="form-text text-muted">La date doit être supérieure ou égale à la date du jour.</small>
                                 </div>
